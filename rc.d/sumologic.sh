f82e421a1e1963560f56904bd594dbf4139f45ec
Rui Coelho <rui.coelho@miniclip.com>
2017-07-14 18:59:14 +0100

    Minor spell check fixes

--- rc.d/sumologic.sh
+++ rc.d/sumologic.sh
@@ -3,7 +3,7 @@ if [ ! -x "$(command -v livetail)" ]; then
     return 1
 fi
 
-# usage: livetail-error soccer_externalevents_v8|soccer-mobile-live-v16
+# usage: livetail-error soccer-externalevents-v8|soccer-mobile-live-v16
 function livetail-error() {
     livetail "_sourceCategory=$1 AND _source=lager_error"
 }
