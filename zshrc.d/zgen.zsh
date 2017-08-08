5ebd37bca863be47c029004ccd352d45cbd03fe8
Rui Coelho <rui.coelho@miniclip.com>
2017-08-08 17:26:16 +0100

    Re-add prezto to avoid re-implementing stuff

--- zshrc.d/zgen.zsh
+++ zshrc.d/zgen.zsh
@@ -1,26 +1,35 @@
-# see https://github.com/tarjoilija/zgen/issues/46
-lazy_source () {
-    eval "$1 () { [ -f $2 ] && source $2 && $1 \$@ }"
-}
-
 # see https://github.com/tarjoilija/zgen
-lazy_source zgen ${HOME}/.zgen/zgen.zsh
+source "${HOME}/.zgen/zgen.zsh"
 # if the init script doesn't exist
-if ! source "$HOME/.zgen/init.zsh"; then
+if ! zgen saved; then
+    echo "Creating a zgen save"
+
+    # default modules
+    zgen prezto
+    #ZGEN_PREZTO_LOAD_DEFAULT=0 # 'environment' 'terminal' 'editor' 'history' 'directory' 'spectrum' 'utility' 'completion' 'prompt'
+
+    # extra modules
+    zgen prezto git
+    zgen prezto fasd
+    zgen prezto homebrew
+    zgen prezto osx
+    zgen prezto autosuggestions
+    #zgen prezto tmux
+
+    # zgen options
+    #zgen prezto editor key-bindings 'vi' # emacs|vi
+    zgen prezto prompt theme 'sorin' # default to sorin
+    zgen prezto utility:ls color 'yes'
+    zgen prezto '*:*' color 'yes'
+    #zgen prezto tmux:auto-start local 'yes'
+    #zgen prezto tmux:iterm integrate 'yes'
+
+    # install some rc prezto files
+    ln -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zprofile" "${ZDOTDIR:-$HOME}/.zprofile"
+    ln -sf "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zshenv" "${ZDOTDIR:-$HOME}/.zshenv"
+
     # see https://github.com/unixorn/awesome-zsh-plugins
-    zgen load horosgrisa/autoenv
-    zgen load miekg/lean
-
-    # sensible defaults
-    zgen load rjcoelho/zsh-saneopt
-
-    # fish shell like syntax highlighting, shell's history search and autosuggestions
-    zgen load zsh-users/zsh-syntax-highlighting
-    zgen load zsh-users/zsh-history-substring-search
-    zgen load zsh-users/zsh-autosuggestions
-
-    # additional completion definitions
-    zgen load zsh-users/zsh-completions
+    zgen load zpm-zsh/autoenv
 
     # save all to init script
     zgen save
