77493fbb299484c63be2116b9cc99a5c81f2ae09
Rui Coelho <rui.coelho@miniclip.com>
2018-10-30 09:39:15 +0000

    Move iterm integration to osx

--- rc.d/osx.sh
+++ rc.d/osx.sh
@@ -1,3 +1,9 @@
 # see https://www.iterm2.com/documentation-shell-integration.html
 [ -f "${HOME}/.iterm2_shell_integration.${SHELL_EXTENSION}" ] && \
     source "${HOME}/.iterm2_shell_integration.${SHELL_EXTENSION}"
+
+
+if [ ! -x "$(command -v osascript)" ]; then
+    return 1
+fi
+alias toggle-dark-mode="osascript -e 'tell app \"System Events\" to tell appearance preferences to set dark mode to not dark mode'"
