80195278809f05fe5a05722a2731b0b65fdab7e7
Rui Coelho <rui.coelho@miniclip.com>
2019-07-15 14:58:04 +0100

    Youtube download playlist in reverse/correct order

--- rc.d/youtube.sh
+++ rc.d/youtube.sh
@@ -8,7 +8,7 @@ export YT_PLAYLIST="https://www.youtube.com/playlist\?list=PLuOtXP9DKvRIJzPbBOS5
 if [ ! -x "$(command -v youtube-dl)" ]; then
     return 1
 fi
-export YT_ARGS="--geo-bypass -q --no-warnings -x --audio-format mp3"
+export YT_ARGS="--geo-bypass --playlist-reverse --no-warnings -x --audio-format mp3"
 alias yt_mp3="youtube-dl $YT_ARGS $YT_PLAYLIST"
 alias yt_plst="youtube-dl -j --flat-playlist $YT_PLAYLIST | jq -r '.id' | sed 's_^_https://youtu.be/_'"
 alias yt_plst_mp3="yt_plst | xargs -n 1 -P 8 youtube-dl $YT_ARGS"
