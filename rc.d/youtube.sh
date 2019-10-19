153e0b5c1520df2cb257714ea2e75a54f2c2b156
Rui Coelho <rui.coelho@miniclip.com>
2019-10-19 15:49:23 +0100

    Update youtube integration

--- rc.d/youtube.sh
+++ rc.d/youtube.sh
@@ -1,14 +1,16 @@
+export YT_PLAYLIST="https://www.youtube.com/playlist?list=PLuOtXP9DKvRIJzPbBOS52rKrKhEbywjwr"
+#export YT_PLAYLIST="https://www.youtube.com/playlist?list=PLuOtXP9DKvRKzc0-_IQpYKefPFxuRKj3I"
+
 # see https://github.com/mps-youtube/mps-youtube
-export YT_PLAYLIST="https://www.youtube.com/playlist\?list=PLuOtXP9DKvRIJzPbBOS52rKrKhEbywjwr"
 [ -x "$(command -v mpsyt)" ] && \
-    alias yt_music="mpsyt pl $YT_PLAYLIST, all repeat"
+    alias yt_music="mpsyt pl \$YT_PLAYLIST, all repeat"
 
 # see https://github.com/ytdl-org/youtube-dl
 # see http://askubuntu.com/questions/178481/how-to-download-an-mp3-track-from-a-youtube-video
 if [ ! -x "$(command -v youtube-dl)" ]; then
     return 1
 fi
-export YT_ARGS="--geo-bypass --playlist-reverse --no-warnings -x --audio-format mp3"
-alias yt_mp3="youtube-dl $YT_ARGS $YT_PLAYLIST"
-alias yt_plst="youtube-dl -j --flat-playlist $YT_PLAYLIST | jq -r '.id' | sed 's_^_https://youtu.be/_'"
+export YT_ARGS="--geo-bypass --playlist-reverse --no-warnings --extract-audio --audio-format mp3 -o '%(title)s.%(ext)s'"
+alias yt_mp3="youtube-dl $YT_ARGS \$YT_PLAYLIST"
+alias yt_plst="youtube-dl -j --flat-playlist \$YT_PLAYLIST | jq -r '.id' | sed 's_^_https://youtu.be/_'"
 alias yt_plst_mp3="yt_plst | xargs -n 1 -P 8 youtube-dl $YT_ARGS"
