d0f322b864fd5e8e5ced860bbe506b0894a04fd6
Rui Coelho <rui.coelho@miniclip.com>
2019-11-30 17:11:22 +0000

    Don't reverse youtube playlist

--- rc.d/youtube.sh
+++ rc.d/youtube.sh
@@ -10,7 +10,7 @@ export YT_PLAYLIST="https://www.youtube.com/playlist?list=PLuOtXP9DKvRIJzPbBOS52
 if [ ! -x "$(command -v youtube-dl)" ]; then
     return 1
 fi
-export YT_ARGS="--geo-bypass --playlist-reverse --no-warnings --extract-audio --audio-format mp3 -o '%(title)s.%(ext)s'"
-alias yt_mp3="youtube-dl $YT_ARGS \$YT_PLAYLIST"
-alias yt_plst="youtube-dl -j --flat-playlist --playlist-reverse \$YT_PLAYLIST | jq -r '.id' | sed 's_^_https://youtu.be/_'"
-alias yt_plst_mp3="yt_plst | xargs -n 1 -P 8 youtube-dl $YT_ARGS"
+export YT_ARGS="--geo-bypass --no-warnings --extract-audio --audio-format mp3 -o '%(title)s.%(ext)s'"
+alias yt_mp3="youtube-dl $YT_ARGS \$YT_PLAYLIST" # download playlist
+alias yt_plst="youtube-dl -j --flat-playlist \$YT_PLAYLIST | jq -r '.id' | sed 's_^_https://youtu.be/_'" # list playlist
+alias yt_plst_mp3="yt_plst | xargs -n 1 -P 8 youtube-dl $YT_ARGS" # download in parallel
