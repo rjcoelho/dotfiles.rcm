c57036cd3a4826e0d46d13b74cf4826489747893
Rui Coelho <rui.coelho@miniclip.com>
2019-12-01 18:08:42 +0000

    Compile rc.d scripts

--- zshrc.d/zim.zsh
+++ zshrc.d/zim.zsh
@@ -1,5 +1,6 @@
 # Define zim location
 export ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim
+export ZIM_CONTRIB=${ZDOTDIR:-${HOME}}/.rc.d
 
 # Start zim
 [[ -s ${ZIM_HOME}/init.zsh ]] && source ${ZIM_HOME}/init.zsh
