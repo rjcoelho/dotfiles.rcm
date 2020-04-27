a8af091f81e7751415f4fca2edbcf4013df931af
Rui Coelho <rui.coelho@miniclip.com>
2020-04-27 10:03:28 +0100

    Add 1password and sonic hammerspoon shortcuts

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -6,12 +6,14 @@ end):start()
 -- Launch or focus on applications
 local mods_app = {'cmd', 'shift'}
 remap = hs.hotkey.bind
+remap(mods_app, '1', function () hs.application.launchOrFocus('1Password 7') end)
 remap(mods_app, 'b', function () hs.application.launchOrFocus('Brave Browser') end)
 remap(mods_app, 'c', function () hs.application.launchOrFocus('Visual Studio Code') end)
 remap(mods_app, 'm', function () hs.application.launchOrFocus('Mail') end)
 remap(mods_app, 'n', function () hs.application.launchOrFocus('Brave Browser Nightly') end)
 remap(mods_app, 's', function () hs.application.launchOrFocus('Slack') end)
 remap(mods_app, 't', function () hs.application.launchOrFocus('iTerm') end)
+remap(mods_app, 'v', function () hs.application.launchOrFocus('SonicWall Mobile Connect') end)
 remap(mods_app, 'y', function () hs.application.launchOrFocus('Kitty') end)
 remap(mods_app, 'z', function () hs.application.launchOrFocus('Zoom.us') end)
 
